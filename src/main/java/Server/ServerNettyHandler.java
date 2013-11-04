622218ceff4d3b9ba4d6c22258508fad0579a2e5
maksimtest <mkn_ua@ukr.net>
2013-11-04 04:19:54 +0200

    5
    5

---
+++ src/main/java/Server/ServerNettyHandler.java
@@ -0,0 +1,190 @@
+package Server;
+
+import java.net.InetAddress;
+import java.net.URI;
+
+import dao.ReportJDBCDao;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpRequest;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import static io.netty.handler.codec.http.HttpHeaders.Names.*;
+import static io.netty.handler.codec.http.HttpHeaders.*;
+import static io.netty.handler.codec.http.HttpResponseStatus.*;
+import static io.netty.handler.codec.http.HttpVersion.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Maksim_Kuzmenyuk
+ * Date: 01.11.13
+ * Time: 18:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class ServerNettyHandler  extends ChannelInboundHandlerAdapter {
+    private boolean isCorrectQuery=false;
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        //Если соединение корректно, то уменьшить количество соединений
+        if(isCorrectQuery)ServerNetty.delCountConnection();
+        isCorrectQuery=false;
+        ctx.flush();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (msg instanceof HttpRequest) {
+            HttpRequest req = (HttpRequest) msg;
+            String uri = req.getUri();
+            String redirectUrl="";
+            if(uri.indexOf("/favicon")>=0){
+                return;
+            }
+            System.out.println("uri="+uri);
+            byte[] responseText="Не корректный запрос. 404".getBytes();
+            String ip = ctx.pipeline().channel().remoteAddress().toString().substring(1);
+            System.out.println("Uri="+uri+" ip="+ip);
+
+            if(uri.indexOf("/hello")>=0){
+                addStat(uri, ip, "");
+                Thread.sleep(10000L);
+                responseText = "Hello World!".getBytes();
+            }
+            else if(uri.indexOf("/redirect?url=")>=0){
+                redirectUrl = uri.substring(14);
+                addStat(uri, ip, redirectUrl);
+            }
+            else if(uri.indexOf("/status")>=0){
+                addStat(uri, ip, "");
+                responseText = getStatus();
+            }
+            // http://localhost:8080/hello
+            // http://localhost:8080/redirect?url=http://bored.ua/result&post=17
+            // http://localhost:8080/status
+            if (is100ContinueExpected(req)) {
+                ctx.write(new DefaultFullHttpResponse(HTTP_1_1, CONTINUE));
+            }
+            boolean keepAlive = isKeepAlive(req);
+            FullHttpResponse response;
+            if(!redirectUrl.equals("")){
+                System.out.println("redirect:"+redirectUrl);
+                response = new DefaultFullHttpResponse(HTTP_1_1,MOVED_PERMANENTLY);
+                response.headers().set(LOCATION, redirectUrl);
+            } else{
+                response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(responseText));
+                response.headers().set(CONTENT_TYPE, "text/html;  charset=UTF-8");
+                response.headers().set(CONTENT_LENGTH, response.content().readableBytes());
+            }
+
+                response.headers().set(CONNECTION, Values.KEEP_ALIVE);
+                ctx.write(response);
+        }
+    }
+
+    private void addStat(String query,String ip,String redirect){
+        System.out.println("!!! "+query);
+        //Увеличение счетчика соединений
+        ServerNetty.addCountConnection();
+        //Установка признака корректности соединения
+        isCorrectQuery=true;
+
+        //Определение текущего времени в формате для записи в БД
+        Date today = new Date();
+        SimpleDateFormat f = new SimpleDateFormat("yyyy.MM.dd hh:mm:ss");
+        String time= f.format(today);
+        //Запись в БД
+        ReportJDBCDao bd = new ReportJDBCDao();
+        new ReportJDBCDao().addReport(query, ip, redirect, time);
+
+    }
+
+    public byte[] getStatus(){
+        ReportJDBCDao bd = new ReportJDBCDao();
+
+        //return String.valueOf(bd.getReport1()).getBytes();
+        //return transformToTable(bd.getReport2());
+        //return transformToTable(bd.getReport3());
+        //return transformToTable(bd.getReport4());
+
+        String s1="<html><title>Статус-отчет</title><body><h1>Статистический отчет</h1>";
+
+        byte[] a = s1.getBytes();
+        byte[] b = transformToTable(bd.getReport1(),"");
+        byte[] c = transformToTable(bd.getReport2(),"");
+        byte[] d = transformToTable(bd.getReport3(),"Счетчик запросов на каждый IP");
+        byte[] e = transformToTable(bd.getReport4(),"Количество переадрессаций по URL");
+        String s2 = "<h3>Количество открытых соединений в данный момент:"+ServerNetty.getCountConnection()+"</h3>";
+        byte[] f = (s2+"</body></html>").getBytes();
+
+        return  joinArrays(a,b,c,d,e,f);
+    }
+    public byte[] transformToTable(ArrayList<ArrayList<String>> mass,String name){
+        String s="<br>";
+        //Если есть заголовок текущей части отчета, то выводить шапку
+        if(!name.equals(""))s= "<h3>"+name+"</h3>";
+
+        byte[] res = (s+"<table style='border:1px solid maroon'>").getBytes();
+        String stroka;
+        for(int i = 0;i<mass.size();i++){
+            stroka = "<tr style='border:1px solid maroon'>";
+            for(int j = 0;j<mass.get(i).size();j++){
+                stroka += "<td style='border:1px solid maroon'>"+mass.get(i).get(j)+"</td>";
+            }
+            stroka += "</tr>";
+            res = joinArrays(res,stroka.getBytes());
+        }
+        res = joinArrays(res,"</table>".getBytes());
+        return res;
+    }
+
+    public byte[] joinArrays(byte[] a,byte[] b){
+        byte[] c= new  byte[a.length+b.length];
+        for(int i = 0;i<a.length;i++){
+            c[i]=a[i];
+        }
+        for(int j = 0;j<b.length;j++){
+            c[j+a.length]=b[j];
+        }
+        return c;
+
+    }
+    public byte[] joinArrays(byte[] a,byte[] b,byte[] c,byte[] d,byte[] e,byte[] f){
+        byte[] r= new  byte[a.length+b.length+c.length+d.length+e.length+f.length];
+        int k=0;
+        for(int i = 0;i<a.length;i++){
+            r[k++]=a[i];
+        }
+        for(int j = 0;j<b.length;j++){
+            r[k++]=b[j];
+        }
+        for(int i = 0;i<c.length;i++){
+            r[k++]=c[i];
+        }
+        for(int j = 0;j<d.length;j++){
+            r[k++]=d[j];
+        }
+        for(int i = 0;i<e.length;i++){
+            r[k++]=e[i];
+        }
+        for(int j = 0;j<f.length;j++){
+            r[k++]=f[j];
+        }
+        return r;
+
+    }
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
+
+
+
